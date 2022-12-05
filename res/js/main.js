a60941617055cab4228dd30d88b2ca730f6a92e5
tomaskocci <tomas.koci@gmail.com>
2022-12-05 10:34:21 +0100

    upgrade js and css

--- res/js/main.js
+++ res/js/main.js
@@ -4,10 +4,43 @@ const zivoty = document.getElementById("zivoty");
 const zivoty2 = document.getElementById("zivoty2");
 const info = document.getElementById("info");
 
+const gohome = document.getElementById("gohome");
+const gogym = document.getElementById("gogym");
+const backongarden = document.getElementById("backongarden");
+
+
+
+
+
+//move 
+gogym.onclick = () =>{
+    garden.style.display= "none";
+    gym.style.display= "block";
+    home.syle.display= "none";
+
+}
+gohome.onclick= () =>{
+    garden.style.display= "none";
+    home.style.display= "block";
+    gym.style.display= "none";
+    
+}
+backongarden.onclick= () =>{
+    home.style.display= "none";
+    garden.style.display= "block";
+    gym.style.display= "none";
+}
+backgarden.onclick = () =>{
+    gym.style.display="none";
+    garden.style.display="block";
+    home.style.display="none";
+
+}
+//atack in gym
 attack.onmousedown = () =>{
 
     if(zivoty2.innerHTML <=0 ){
-        info.innerHTML= "nepřítel zemřel";
+        info.innerHTML= "dodělal jsi sérii";
         clearInterval(interval)
     }else if(zivoty.innerHTML>0){
         div.style.left = "200px";
@@ -21,8 +54,12 @@ attack.onmouseup = ( ) =>{
 const interval = setInterval(()=>{
     zivoty.innerHTML--;
     if(zivoty.innerHTML <=0){
-        info.innerHTML="zemřel jsi"
+        info.innerHTML="neudělal jsi sérii"
         clearInterval(interval);
     }
 
 },500)
+
+
+
+
